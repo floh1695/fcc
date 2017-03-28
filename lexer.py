c2d1e438a3e376b46635cfde711e87458a3a13ae
Charles Bayley <cbayley95@gmail.com>
2017-03-28 13:36:45 -0500

    Can now sort param chunks out

--- lexer.py
+++ lexer.py
@@ -2,6 +2,10 @@
 
 import string
 
+T_BLOCK = 'block.{}'
+T_BLOCK_CURLY = T_BLOCK.format('curly')
+T_BLOCK_SQUARE = T_BLOCK.format('square')
+T_BLOCK_PARAM = T_BLOCK.format('param')
 T_COMMENT = 'comment'
 T_STRING = 'string'
 T_WORD = 'word'
@@ -11,11 +15,24 @@ class Token:
         self.mtype = mtype
         self.word = word
 
+    @staticmethod
+    def name_block(block_char):
+        if block_char in ['{', '}']:
+            return 'curly'
+        elif block_char in ['[', ']']:
+            return 'square'
+        elif block_char in ['(', ')']:
+            return 'param'
+        raise Exception('Not a valid block char: \"{}[]()\"')
+
     def __str__(self):
         return 'Token({}, \"{}\")'.format(self.mtype, self.word)
 
     def __repr__(self):
         return str(self)
+
+class EndSorting(Exception):
+    pass
 
 class Lexer:
     def __init__(self, filename):
@@ -69,8 +86,10 @@ class Lexer:
         for char in self._char_gen:
             self._tokenize_sorter(char)
 
-    def _tokenize_sorter(self, char):
-        if char in ['#']:
+    def _tokenize_sorter(self, char, s_char=None):
+        if char == s_char:
+            raise EndSorting()
+        elif char in ['#']:
             self._tokenize_comment()
         elif char in ['\"', '`', '\'']:
             self._tokenize_string(char)
@@ -78,6 +97,8 @@ class Lexer:
             return
         elif char in (string.ascii_lowercase + string.ascii_uppercase):
             self._tokenize_word(char)
+        elif char in ['(', '{', '[']:
+            self._tokenize_param(char)
 
     def _tokenize_comment(self):
         for char in self._char_gen:
@@ -103,3 +124,23 @@ class Lexer:
                 self._push_word(T_WORD)
                 self._tokenize_sorter(char)
                 return
+
+    # TODO: Clean up this messy method
+    def _tokenize_param(self, param):
+        e_param = ''
+        if param == '(':
+            e_param = ')'
+        elif param == '{':
+            e_param = '}'
+        elif param == '[':
+            e_param = ']'
+        stack = self.token_stack
+        self.token_stack = []
+        for char in self._char_gen:
+            try:
+                self._tokenize_sorter(char, e_param)
+            except EndSorting, e:
+                break
+        token = Token(T_BLOCK.format(Token.name_block(param)), self.token_stack)
+        self.token_stack = stack
+        self._push_token(token)
